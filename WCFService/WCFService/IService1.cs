3dcb453686a829ac58ec523a92679b5075ca559e
El-tigree <tshenolopooe@gmail.com>
2016-06-22 20:00:31 +0200

    andriod webservice

--- WCFService/WCFService/IService1.cs
+++ WCFService/WCFService/IService1.cs
@@ -8,11 +8,11 @@ using System.Text;
 
 namespace WCFService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    [ServiceContract]
-    public interface IService1
-    {
-        [OperationContract]
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IService1
+	{
+		[OperationContract]
         string getPackage(int id);
 
         [OperationContract]
