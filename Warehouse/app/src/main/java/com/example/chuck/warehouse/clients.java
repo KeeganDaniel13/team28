c1c3617eee153ea6f73a45ad9b1c003a069f3d8e
chuckmulatto <kmalende@gmail.com>
2016-06-24 10:54:19 +0200

    mobile application

---
+++ Warehouse/app/src/main/java/com/example/chuck/warehouse/clients.java
@@ -0,0 +1,13 @@
+package com.example.chuck.warehouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clients);
+    }
+}
