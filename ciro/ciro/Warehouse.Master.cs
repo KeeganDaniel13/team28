1667c3454bbb85ccf4a48d9c8f508a9e64e1471e
JoshuaKinnear95 <JoshuaKinnear.jk@gmail.com>
2016-06-28 14:11:19 +0200

    master pages for warehouse and admin

---
+++ ciro/ciro/Warehouse.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ciro
+{
+    public partial class Warehouse : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
