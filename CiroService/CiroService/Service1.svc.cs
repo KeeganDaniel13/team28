a70b9dff68876c2f6b4d79d58da94d57cb9bbe00
El-Tigree <tshenolopooe@gmail.com>
2016-07-12 15:55:39 +0200

    Fixing merge errors

--- CiroService/CiroService/Service1.svc.cs
+++ CiroService/CiroService/Service1.svc.cs
@@ -7,56 +7,18 @@ using System.ServiceModel.Web;
 using System.Text;
 using CiroService.EFDatabase;
 using CiroService.DAL;
-<<<<<<< HEAD
-=======
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using CiroService.JsonObjects;
 using System.Web.Http;
 using System.Web;
 
->>>>>>> 8f6445e3901c44fb9322d5e00765e85cffa47959
 namespace CiroService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-<<<<<<< HEAD
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public string login(string name,string password)
-        {
-            var userAccess = new userController();
-            IEnumerable<user> users = userAccess.getTable();
-            var user = users.FirstOrDefault<user>(c => c.user_fname.Contains(name) && c.user_password.Contains(password));
-            if (user != null)
-            {
-                return "user exists";
-            }
-            return "user doesn't exist";
-        }
-
-        public IEnumerable<product> listProduct()
-        {
-            return null;
-        }
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-=======
 
         public string GetData()
         {
@@ -143,6 +105,6 @@ namespace CiroService
         //}
         //return composite;
         // }
->>>>>>> 8f6445e3901c44fb9322d5e00765e85cffa47959
+
     }
 }
