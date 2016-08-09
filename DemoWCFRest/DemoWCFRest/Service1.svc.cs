359c218972dfe56cb04b18e1022b9b0195d845b4
El-Tigree <tshenolopooe@gmail.com>
2016-08-09 10:01:41 +0200

    merging newBranchService

---
+++ DemoWCFRest/DemoWCFRest/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.ServiceModel.Activation;
+
+namespace DemoWCFRest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1 : IService1
+    {
+        public string DoWork(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public int DoSquare(int value)
+        {
+
+            return value * value;
+        }
+
+        public int DoAdd(AddValues values)
+        {
+            return values.value1 + values.value2;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
